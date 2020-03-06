2352ba9ee656ef0fbae8b655700703383dbb5ca9
Xrhoys <shuaiting.zheng@efrei.net>
2020-03-06 19:21:50 +0100

    fix database

--- dbHelpers/mySqlWrapper.js
+++ dbHelpers/mySqlWrapper.js
@@ -3,7 +3,9 @@ const mysql = require('mysql2')
 let connection = null
 
 const initConnection = () => {
-    connection = mysql.createConnection({
+    connection = mysql.createPool({
+        connectionLimit: 10, //important,
+        "socketPath": "/cloudsql/planeat-nodejs-backend:europe-west1:planeat-my-sql-instance",
         // host: 'remotemysql.com',
         // user: '5XNzHryt72',
         // password: 'w9Pq7D4il6',
@@ -12,10 +14,9 @@ const initConnection = () => {
         username: "will2106",
         password: "6995",
         database: "planeat_db",
-        host: "/cloudsql/planeat-nodejs-backend:europe-west1:planeat-my-sql-instance",
-        "dialectOptions": {
-            "socketPath": "/cloudsql/planeat-nodejs-backend:europe-west1:planeat-my-sql-instance"
-        },
+        // "dialectOptions": {
+        //     "socketPath": "/cloudsql/planeat-nodejs-backend:europe-west1:planeat-my-sql-instance"
+        // },
         dialect: "mysql"
     })
 }
