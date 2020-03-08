9dedcbe4ea231a72d9e7407415bab1685a63cd16
Xrhoys <shuaiting.zheng@efrei.net>
2020-03-08 18:23:22 +0100

    fix server port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./index')
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
