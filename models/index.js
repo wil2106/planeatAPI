2c4f1b3f944f92420499b03c657b57beafc12ffd
Xrhoys <shuaiting.zheng@efrei.net>
2020-03-08 16:05:16 +0100

    fix sequelize sql instance

--- models/index.js
+++ models/index.js
@@ -12,7 +12,18 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password, {
+      dialect: 'mysql',
+      host: config.host,
+      dialectOptions: {
+        socketPath: config.dialectOptions.socketPath
+      },
+      pool: config.pool
+    }
+  );
 }
 
 fs
