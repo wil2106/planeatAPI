2d1c91f2163b8c5e569504edb66317044b127218
wil2106 <serviantwilliam@gmail.com>
2020-03-11 20:45:36 +0100

    Recuperation des variables d environnement sur un bucket gcp

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+console.log(process.env.DB_HOST)
 const express = require('express')
 const bodyParser = require('body-parser')
 const mySqlConnection = require('./dbHelpers/mySqlWrapper')
