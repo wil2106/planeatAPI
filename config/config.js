f0cee696cd9f4c9a09d8d02fe5e730958053a591
wil2106 <serviantwilliam@gmail.com>
2020-03-12 21:46:27 +0100

    Recuperation des variables d environnement sur un bucket gcp

--- config/config.js
+++ config/config.js
@@ -1,10 +1,5 @@
 require('dotenv').config()
 
-console.log(`DB_USER: ${process.env.DB_USER}`)
-console.log(`DB_PASS: ${process.env.DB_PASS}`)
-console.log(`DB_HOST: ${process.env.DB_HOST}`)
-console.log(`DB_PORT: ${process.env.DB_PORT}`)
-console.log(`DB_DATABASE: ${process.env.DB_DATABASE}`)
 const envVar = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
