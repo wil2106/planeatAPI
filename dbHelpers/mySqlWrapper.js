6b6eb2c35e6db8481dfb4ee36115dfd68f9d4bfd
wil2106 <serviantwilliam@gmail.com>
2020-03-18 11:52:58 +0100

    test connexion

--- dbHelpers/mySqlWrapper.js
+++ dbHelpers/mySqlWrapper.js
@@ -4,6 +4,10 @@ const config = require(__dirname + '/../config/config');
 let connection = null
 
 const initConnection = () => {
+    console.log(config.cloud_username)
+    console.log(config.cloud_password)
+    console.log(config.cloud_database)
+    console.log(config.cloud_connectionName)
     connection = mysql.createConnection({
         connectionLimit: 10,
         host: `${config.host}`,
